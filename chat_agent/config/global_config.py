81df140caa4e10bd2932ae2499e4c82fab48b39d
csmn <1154027137@qq.com>
2024-10-23 23:43:18 +0800

    update config

--- chat_agent/config/global_config.py
+++ chat_agent/config/global_config.py
@@ -68,7 +68,7 @@ cot_config = dict(
     """,
     query_prompt_group = """
         现在的时间是<{system_time}>, 用户<{user_name}>在群里说：{user_message}
-        群里的发言较多，如果你没有非常想说的话，请调用 bot_ignore 保持沉默。
+        群里的发言较多，如果大家没有和你说话，请调用 bot_ignore 保持沉默。
         但是如果用户主动叫你的名字，请回答用户的问题。
         此外，如果用户的认知存在问题，你需要指出。
     """,
