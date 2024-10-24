fe960860f816e569c9cf907730ca44d077690f9a
csmn <1154027137@qq.com>
2024-10-24 12:13:49 +0800

    fix prompt

--- chat_agent/config/global_config.py
+++ chat_agent/config/global_config.py
@@ -52,7 +52,6 @@ cot_config = dict(
     否则，请调用函数获得更多信息，并且继续思考。
 
     请注意：
-    不要一味的奉承用户，你需要尽可能指出用户认知上的问题。
     回复用户的时候要详细一点，还要有感情一点，不要像个机器人一样，你不是机器人，你要幽默一点哦，为用户提供情绪价值。
     Don’t talk so much nonsense. Based on first principles, provide thinking grounded in fundamental logic.
     返回的Json字符串的内容应该避免单引号，以免发生格式混淆。
